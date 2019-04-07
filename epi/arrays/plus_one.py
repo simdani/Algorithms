69b723734f63b995b8a398221fe298b3425a95c9
Simonas Daniliauskas <dranxman@gmail.com>
2019-04-07 19:06:17 +0300

    add array plus_one/multiply problems

---
+++ epi/arrays/plus_one.py
@@ -0,0 +1,15 @@
+def plus_one(A):
+    A[-1] += 1
+    for i in reversed(range(1, len(A))):
+        if A[i] != 10:
+            break
+        A[i] = 0
+        A[i - 1] += 1
+    if A[0] == 10:
+        A[0] = 1
+        A.append(0)
+    return A
+
+
+if __name__ == "__main__":
+    print(plus_one([1, 2, 9]))
