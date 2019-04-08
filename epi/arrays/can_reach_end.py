899e2d4ea614a5e8e8a699dbdcae7c2e65f8a318
Simonas Daniliauskas <dranxman@gmail.com>
2019-04-08 21:41:59 +0300

    add more problems with arrays

---
+++ epi/arrays/can_reach_end.py
@@ -0,0 +1,7 @@
+def can_reach_end(A):
+    furthest_reach_so_far, last_index = 0, len(A) - 1
+    i = 0
+    while i <= furthest_reach_so_far and furthest_reach_so_far < last_index:
+        furthest_reach_so_far = max(furthest_reach_so_far, A[i] + 1)
+        i += 1
+    return furthest_reach_so_far >= last_index
