7533fd00eb2f16cfe84600515623921017d71f39
Simonas Daniliauskas <daniliauskas.s@gmail.com>
2019-10-07 22:58:29 +0300

    add 67-add-binary problem

---
+++ leetcode/swift/67-Add-Binary/MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func addBinary(_ a: String, _ b: String) -> String {
+      var a = a
+      var b = b
+      var carry = 0
+      var result = ""
+      while !a.isEmpty || !b.isEmpty || carry > 0 {
+        let x = Int(a.isEmpty ? "0" : String(a.removeLast()))!
+        let y = Int(b.isEmpty ? "0" : String(b.removeLast()))!
+        let sum = x + y + carry
+        result = String(sum % 2) + result
+        carry = sum / 2
+      }
+
+      return result
+    }
+}
+
+debugPrint(Solution().addBinary("11", "1"))
