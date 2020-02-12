0eae55e882105b24d0296c0a756474cf2cd61ff0
Simonas Daniliauskas <daniliauskas.s@gmail.com>
2020-02-12 20:31:39 +0200

    add 70 climbing stairs swift solution

---
+++ leetcode/swift/70-climbing-stairs/MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+class Solution {
+    private var memoizedResult = [Int:Int]()
+
+    func climbStairs(_ n : Int) -> Int {
+        if n == 0 || n == 1 {
+            return 1
+        }
+
+        if let memoizedValue = memoizedResult[n] {
+            return memoizedValue
+        }
+
+        let result = climbStairs(n - 1) + climbStairs(n - 2)
+        memoizedResult[n] = result
+        return result
+    }
+}
+
+print(Solution().climbStairs(2))
+print(Solution().climbStairs(3))
